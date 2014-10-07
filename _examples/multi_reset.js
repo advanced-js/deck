cffe4a3e5d0e2fb0b65dec8c1f2d1408dc5798f2
Aidan Feldman <aidan.feldman@gmail.com>
2014-10-07 11:47:15 -0400

    add multi_reset example

---
+++ _examples/multi_reset.js
@@ -0,0 +1,18 @@
+---
+---
+
+function reset(statusObj){
+ statusObj.counter = 0;
+}
+
+var myStatus1 = {
+ counter: 6
+};
+var myStatus2 = {
+ counter: 6
+};
+
+reset(myStatus1);
+
+assertTripleEqual(myStatus1, { counter: 0 }, "Object passed to reset() gets modified");
+assertTripleEqual(myStatus2, { counter: 6 }, "Unmodified, because objects looking the same doesn't mean they are the same place in memory");
