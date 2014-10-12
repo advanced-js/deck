bfeadcbee7eefd58f57c1f87558166361c4f5934
Aidan Feldman <aidan.feldman@gmail.com>
2014-10-11 23:42:24 -0400

    use built-in bind()

--- _examples/bind.js
+++ _examples/bind.js
@@ -9,19 +9,15 @@ var obj = {
   }
 };
 
-function bind(fn, context){
-  return function(){
-    return fn.apply(context, arguments);
-  };
-}
-
 // alias the function
 var unbound = obj.fullName;
 
 assertTripleEqual(unbound(), 'undefined undefined', "without binding, function calls use the calling scope");
 
 // bind it to the original context
-var bound = bind(unbound, obj);
+// note this is an ES5 function:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind#Browser_compatibility
+var bound = unbound.bind(obj);
 
 assertTripleEqual(typeof bound, 'function', "bind() returns a function");
 assertTripleEqual(bound === unbound, false, "returns a new function");
