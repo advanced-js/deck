15ad02a329a174ba77dcdbc4afe106504fa7f386
Aidan Feldman <aidan.feldman@gmail.com>
2014-10-12 18:52:55 -0400

    add slide on double vs triple-equal

---
+++ _examples/double_vs_triple_equal.js
@@ -0,0 +1,14 @@
+---
+---
+
+assertTripleEqual(1 == 1, true);
+assertTripleEqual(1 === 1, true);
+
+assertTripleEqual('foo' == 'foo', true);
+assertTripleEqual('foo' === 'foo', true);
+
+assertTripleEqual(null == undefined, true, "double-equals coerces types to match");
+assertTripleEqual(null === undefined, false, "triple-equals does strict type checking");
+
+assertTripleEqual({} == {}, false);
+assertTripleEqual({} === {}, false);
