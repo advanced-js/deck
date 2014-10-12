c3aaaffdb19fc1609570199e56a086050b220535
Aidan Feldman <aidan.feldman@gmail.com>
2014-10-11 23:57:38 -0400

    remove weird use of .apply()

--- _examples/apply_context.js
+++ _examples/apply_context.js
@@ -1,18 +1,14 @@
 ---
 ---
 
-// NOTE: broken in the deck
-
 var myVariable = "I'm global!";
 
 function printMyVariable(){
   return this.myVariable;
 }
 
-// without additional arguments, apply() vs. call() are interchangeable
+// without additional arguments, apply() & call() are interchangeable
 assertTripleEqual(printMyVariable(), "I'm global!", "without specifying context");
-assertTripleEqual(printMyVariable.apply(null), "I'm global!", "using 'null' context");
-assertTripleEqual(printMyVariable.apply(undefined), "I'm global!", "using 'undefined' context");
 assertTripleEqual(printMyVariable.apply(this), "I'm global!", "using 'this' context");
 
 assertTripleEqual(printMyVariable.apply({ myVariable: "I'm local!" }), "I'm local!", "using object as context");
