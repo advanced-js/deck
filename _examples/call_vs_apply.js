ab6677629f987965f0ec8451bbbe43c391b07e4f
Aidan Feldman <aidan.feldman@gmail.com>
2014-10-12 01:20:43 -0400

    include a custom context when showing call vs apply

--- _examples/call_vs_apply.js
+++ _examples/call_vs_apply.js
@@ -1,11 +1,15 @@
 ---
 ---
 
-function title(first, last){
-  return first + ' ' + last + ', M.D.';
+function titleize(first, last){
+  return first + ' ' + last + ', ' + this.doctorSuffix;
 }
 
-assertTripleEqual(title.call(this, 'Michaela', 'Quinn'), 'Michaela Quinn, M.D.', "call() takes individual arguments after the context");
-assertTripleEqual(title.apply(this, ['Michaela', 'Quinn']), 'Michaela Quinn, M.D.', "apply() takes individual arguments after the context");
+var context = {
+  doctorSuffix: 'M.D.'
+};
+
+assertTripleEqual(titleize.call(context, 'Michaela', 'Quinn'), 'Michaela Quinn, M.D.', "call() takes individual arguments after the context");
+assertTripleEqual(titleize.apply(context, ['Michaela', 'Quinn']), 'Michaela Quinn, M.D.', "apply() takes individual arguments after the context");
 
 // easy to remember - 'a' (in 'apply') is for Array
