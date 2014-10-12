41f982ecd961135b398c1aa4469f54be5adc7df1
Aidan Feldman <aidan.feldman@gmail.com>
2014-10-12 00:02:12 -0400

    show that argument elements can be accessed like an array

--- _examples/arguments.js
+++ _examples/arguments.js
@@ -5,7 +5,8 @@ function argumentative(){
   assertTripleEqual(arguments instanceof Array, false, "'arguments' isn't really an array");
   assertTripleEqual(typeof arguments.join, 'undefined', "no join method on 'arguments'");
 
-  assertTripleEqual(arguments.length, 3, "but 'arguments' do have 'length'");
+  assertTripleEqual(arguments.length, 3, "but 'arguments' do have .length");
+  assertTripleEqual(arguments[1], 'bar', "they can access elements like an array");
 }
 
 argumentative('foo', 'bar', 'baz');
