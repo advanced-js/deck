a87e2a0fdcd49ae5cff9ffd5aa56c0be462c1503
Aidan Feldman <aidan.feldman@gsa.gov>
2015-04-16 14:40:44 -0400

    split array_as_object slide

---
+++ _examples/array_access.js
@@ -0,0 +1,24 @@
+---
+---
+
+var words = [
+  'foo',
+  'bar',
+  'baz'
+];
+
+// how do you access items in an Array?
+
+
+
+
+
+
+
+
+
+
+
+
+
+assertTripleEqual(words[0], 'foo', "access with number");
--- _examples/array_as_object.js
+++ _examples/array_as_object.js
@@ -1,19 +1,16 @@
 ---
 ---
 
-var myAry = [
+var words = [
   'foo',
   'bar',
   'baz'
 ];
+var properties = {};
 
-assertTripleEqual(myAry[0], 'foo', "access with number");
-assertTripleEqual(myAry['0'], 'foo', "access with string");
+assertTripleEqual(typeof properties, 'object', "an object is an Object");
+assertTripleEqual(typeof words, 'object', "so is an Array");
 
-assertTripleEqual(typeof {}, 'object', "an object is an Object");
-assertTripleEqual(typeof myAry, 'object', "so is an Array");
-
-myAry.newProp = "A property on an array";
-assertTripleEqual(myAry.newProp, "A property on an array", "assigning arbitrary properties");
-assertTripleEqual(myAry['newProp'], "A property on an array", "can retrieve properties with square brackets");
-assertTripleEqual(myAry[0], 'foo', "array access still works");
+words.newProp = "A property on an array";
+assertTripleEqual(words.newProp, "A property on an array", "assigning arbitrary properties");
+assertTripleEqual(words['newProp'], "A property on an array", "can retrieve properties with square brackets");
