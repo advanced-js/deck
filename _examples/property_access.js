67645296821aeb551a9a37bc20f66e456222693d
Aidan Feldman <aidan.feldman@gsa.gov>
2015-04-16 15:09:16 -0400

    add propoerty access slide
    closes https://github.com/advanced-js/syllabus/issues/28

---
+++ _examples/property_access.js
@@ -0,0 +1,15 @@
+---
+---
+
+var user = {
+  first: 'Bob',
+  last: 'Hope'
+};
+
+// What do you call the thing above?
+/* A: An object literal. */
+
+// How can you access the first name?
+/* A: `user.first`, or `user['first']`. */
+
+// See http://stackoverflow.com/q/4968406/358804
